2646cd9c4b5851917a8ca220fefe9fa87d6c7f6e
theang-dyna <theangdyna365@gmail.com>
2024-12-18 15:32:58 +0700

    cofig sst by boostrap cmd

--- sst.config.ts
+++ sst.config.ts
@@ -1,15 +1,15 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-//
+
 export default $config({
   app(input) {
     return {
-      name: "sst-app",
+      name: "sst-deploy-nextjs",
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
     };
   },
   async run() {
-    new sst.aws.Nextjs("testApp");
+    new sst.aws.Nextjs("MyWeb");
   },
 });
